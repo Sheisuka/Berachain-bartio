d145d916a5517b25d12e4f04ce9ff8259b65ca52
sheisuka <xxtornex@gmail.com>
2024-08-26 21:19:26 +0700

    Add modules

---
+++ modules/bex.py
@@ -0,0 +1,13 @@
+import json
+
+
+class Bex:
+    def __init__(self, connection):
+        self.connection = connection
+        self.address = "fd"
+
+    def swap(self):
+        ...
+    
+    def add_liquidity(self):
+        ...
