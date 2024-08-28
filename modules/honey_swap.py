00b8cc59d0749af4987a0d779cc718b2e4bf68d1
sheisuka <xxtornex@gmail.com>
2024-08-29 00:54:27 +0700

    Add error handlers

--- modules/honey_swap.py
+++ modules/honey_swap.py
@@ -1,46 +1,68 @@
-import json
+import web3
+
+import settings
+import modules.module
+import modules.utility
+import modules.handlers
 
 
-class HoneySwap:
-    def __init__(self, connection):
-        self.connection = connection
-        self.address = "0xAd1782b2a7020631249031618fB1Bd09CD926b31"
-        honey_factory_abi_file = open("berakiller/abis/core/HoneyFactory.json")
-        honey_factory_abi = json.load(honey_factory_abi_file)
-        honey_factory_abi_file.close()
-        self.contract = connection.eth.contract(address=self.address, abi=honey_factory_abi)
+class HoneySwap(modules.module.Module):
+    def __init__(self, w3: web3.Web3):
+        super().__init__(w3, settings.HONEYSWAP_ADDRESS, settings.HONEYSWAP_ABI_PATH)
 
-    def mint(self, account, asset, amount):
-        from_ = account.address
+    def mint(self, account: web3.Account, asset: str, amount: int) -> None:
+        @modules.handlers.error_handler
+        def _approve():
+            usdc_contract = modules.utility._get_contract(self.w3, settings.USDC_ABI_PATH, settings.USDC_ADDRESS)
+            tx = usdc_contract.functions.approve(self.address, amount).build_transaction({
+                'gas': 500000,
+                'nonce': self.w3.eth.get_transaction_count(account.address),
+                'maxFeePerGas': 200000000,
+                'maxPriorityFeePerGas': 100000000,
+            })
+            signed_tx = self.w3.eth.account.sign_transaction(tx, private_key=account.key)
+            tx_hash = self.w3.eth.send_raw_transaction(signed_tx.raw_transaction)
+            self.w3.eth.wait_for_transaction_receipt(tx_hash)
 
-        nonce = self.connection.eth.get_transaction_count(from_)
-        usdc_abi_file = open("berakiller/abis/core/USDC.json")
-        usdc_abi = json.load(usdc_abi_file)
-        usdc_abi_file.close()
-        usdc_contract = self.connection.eth.contract(address="0xd6D83aF58a19Cd14eF3CF6fe848C9A4d21e5727c", abi=usdc_abi)
-        tx = usdc_contract.functions.approve(self.address, amount).build_transaction({
-            'from': from_,
-            'nonce': nonce,
-            'gas': 500000,
-            'maxFeePerGas': 200000000,
-            'maxPriorityFeePerGas': 100000000,
-        })
-        signed_tx = self.connection.eth.account.sign_transaction(tx, private_key=account.key)
-        tx_hash = self.connection.eth.send_raw_transaction(signed_tx.raw_transaction)
-        print("Approved - ", tx_hash.hex())
-        tx_receipt = self.connection.eth.wait_for_transaction_receipt(tx_hash)
+        @modules.handlers.error_handler
+        def _mint():
+            tx = self.contract.functions.mint(asset, amount, account.address).build_transaction({
+                'gas': 500000,
+                'nonce': self.w3.eth.get_transaction_count(account.address),
+                'maxFeePerGas': 200000000,
+                'maxPriorityFeePerGas': 100000000,
+            })
+            signed_tx = self.w3.eth.account.sign_transaction(tx, private_key=account.key)
+            self.w3.eth.send_raw_transaction(signed_tx.raw_transaction)
+        
+        _approve()
+        _mint()
 
-        nonce = self.connection.eth.get_transaction_count(from_)
-        tx = self.contract.functions.mint(asset, amount, from_).build_transaction({
-            'from': from_,
-            'nonce': nonce,
-            'gas': 500000,
-            'maxFeePerGas': 200000000,
-            'maxPriorityFeePerGas': 100000000,
-        })
-        signed_tx = self.connection.eth.account.sign_transaction(tx, private_key=account.key)
-        tx_hash = self.connection.eth.send_raw_transaction(signed_tx.raw_transaction)
-        print("Minted - ", tx_hash.hex())
 
-    def reedem(self):
-        ...
+    def reedem(self, account: web3.Account, asset: str, amount: int) -> None:
+        @modules.handlers.error_handler
+        def _approve():
+            honey_contract = modules.utility._get_contract(self.w3, settings.HONEY_ABI_PATH, settings.HONEY_ADDRESS)
+            tx = honey_contract.functions.approve(self.address, amount).build_transaction({
+                'gas': 500000,
+                'nonce': self.w3.eth.get_transaction_count(account.address),
+                'maxFeePerGas': 200000000,
+                'maxPriorityFeePerGas': 100000000,
+            })
+            signed_tx = self.w3.eth.account.sign_transaction(tx, private_key=account.key)
+            tx_hash = self.w3.eth.send_raw_transaction(signed_tx.raw_transaction)
+            self.w3.eth.wait_for_transaction_receipt(tx_hash)
+        
+        @modules.handlers.error_handler
+        def _mint():
+            tx = self.contract.functions.redeem(asset, amount, account.address).build_transaction({
+                'gas': 500000,
+                'nonce': self.w3.eth.get_transaction_count(account.address),
+                'maxFeePerGas': 200000000,
+                'maxPriorityFeePerGas': 100000000,
+            })
+            signed_tx = self.w3.eth.account.sign_transaction(tx, private_key=account.key)
+            self.w3.eth.send_raw_transaction(signed_tx.raw_transaction)
+
+        _approve()
+        _mint()
