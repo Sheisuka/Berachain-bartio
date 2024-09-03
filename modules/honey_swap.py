033ebfc09dcac39ec1a2397a8a00822fee9378b0
sheisuka <xxtornex@gmail.com>
2024-09-03 23:09:00 +0700

    Refactor handlers

--- modules/honey_swap.py
+++ modules/honey_swap.py
@@ -10,57 +10,28 @@ class HoneySwap(modules.module.Module):
     def __init__(self, w3: web3.Web3):
         super().__init__(w3, settings.DAPPS["HONEYSWAP"]["ADDRESS"], settings.DAPPS["HONEYSWAP"]["ABI_PATH"])
 
-    def mint(self, account: web3.Account, asset: str, amount: int) -> None:
-        @modules.handlers.exception_handler
-        def _approve():
-            usdc_contract = modules.utility._get_contract(self.w3, settings.TOKENS["USDC"]["ABI_PATH"], settings.TOKENS["USDC"]["ADDRESS"])
-            if usdc_contract.functions.allowance(account.address, self.address).call() >= amount:
-                return
-            tx = usdc_contract.functions.approve(self.address, amount).build_transaction({
-                'gas': 500000,
-                'nonce': self.w3.eth.get_transaction_count(account.address),
-                'maxFeePerGas': 200000000,
-                'maxPriorityFeePerGas': 100000000,
-            })
-            self._send_tx_and_wait(account, tx)
+    def mint_honey(self, account: web3.Account, base: str, amount: int) -> None:        
+        self._approve(account, base, amount)
+        self._mint_honey(account, base, amount)
 
-        @modules.handlers.exception_handler
-        def _mint():
-            tx = self.contract.functions.mint(asset, amount, account.address).build_transaction({
-                'gas': 500000,
-                'nonce': self.w3.eth.get_transaction_count(account.address),
-                'maxFeePerGas': 200000000,
-                'maxPriorityFeePerGas': 100000000,
-            })
-            self._send_tx_and_wait(account, tx)
-        
-        _approve()
-        _mint()
-
-
-    def reedem(self, account: web3.Account, asset: str, amount: int) -> None:
-        @modules.handlers.exception_handler
-        def _approve():
-            honey_contract = modules.utility._get_contract(self.w3, settings.TOKENS["HONEY"]["ABI_PATH"], settings.TOKENS["HONEY"]["ADDRESS"])
-            if honey_contract.functions.allowance(account.address, self.address).call() >= amount:
-                return
-            tx = honey_contract.functions.approve(self.address, amount).build_transaction({
-                'gas': 500000,
-                'nonce': self.w3.eth.get_transaction_count(account.address),
-                'maxFeePerGas': 200000000,
-                'maxPriorityFeePerGas': 100000000,
-            })
-            self._send_tx_and_wait(account, tx)
-        
-        @modules.handlers.exception_handler
-        def _mint():
-            tx = self.contract.functions.redeem(asset, amount, account.address).build_transaction({
-                'gas': 500000,
-                'nonce': self.w3.eth.get_transaction_count(account.address),
-                'maxFeePerGas': 200000000,
-                'maxPriorityFeePerGas': 100000000,
-            })
-            self._send_tx_and_wait(account, tx)
-
-        _approve()
-        _mint()
+    def reedem_for_honey(self, account: web3.Account, quote: str, amount: int) -> None:
+        self._approve(account, quote, amount)
+        self._redeem_for_honey(account, quote, amount)
+    
+    def _mint_honey(self, account: web3.Account, base: str, amount: int) -> None:
+        tx = self.contract.functions.mint(base, amount, account.address).build_transaction({
+            'gas': 500000,
+            'nonce': self.w3.eth.get_transaction_count(account.address),
+            'maxFeePerGas': 200000000,
+            'maxPriorityFeePerGas': 100000000,
+        })
+        self._send_tx_and_wait(account, tx)
+    
+    def _redeem_for_honey(self, account: web3.Account, quote: str, amount: int) -> None:
+        tx = self.contract.functions.redeem(quote, amount, account.address).build_transaction({
+            'gas': 500000,
+            'nonce': self.w3.eth.get_transaction_count(account.address),
+            'maxFeePerGas': 200000000,
+            'maxPriorityFeePerGas': 100000000,
+        })
+        self._send_tx_and_wait(account, tx)
