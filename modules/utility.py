5117ec788ffa9acfd4976aef02353553dce6adcb
sheisuka <xxtornex@gmail.com>
2024-09-03 23:21:21 +0700

    Change args order

--- modules/utility.py
+++ modules/utility.py
@@ -13,7 +13,7 @@ def _load_abi(abi_path: str) -> typing.Any:
     with path.open() as abi_file:
         return json.load(abi_file)
     
-def _get_contract(w3: web3.Web3, abi_path: str, address: str) -> web3.contract.Contract:
+def _get_contract(w3: web3.Web3, address: str, abi_path: str) -> web3.contract.Contract:
     """Create contract with address and ABI"""
     abi = _load_abi(abi_path)
     return w3.eth.contract(address=address, abi=abi)
